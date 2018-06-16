3ee8c311e81ab6f2caeda13a5866adc050bf9be2
Andrea Merlin <merlin.andrea@gmail.com>
2018-06-16 14:07:43 +0200

    add reset button

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -20,13 +20,26 @@ export class UserDetailComponent implements OnInit {
 
   // save user
   saveUser() {
-    if (this.user.id > 0) {                       // Edit user
-      this.userService.updateUser(this.user);
-    }
+
     if (this.user.id === 0) {
       this.userService.createUser(this.user);     // New user
     }
 
+    if (this.user.id > 0) {                       // Edit user
+      this.userService.updateUser(this.user);
+    }
+
+  }
+
+  // reset form
+  // use ngForm in html #f="ngForm"
+  resetForm(form) {
+      if ( this.user.id === 0 ) {     // reset new user
+        this.user = new User();
+      }
+      if ( this.user.id > 0 ) {       // reset exit user
+          form.reset();
+      }
   }
 
 }
